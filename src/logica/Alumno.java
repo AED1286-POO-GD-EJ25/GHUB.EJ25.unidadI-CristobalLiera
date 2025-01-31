ae32f0d7edfff429decf040c37da24580bd842fa
Cristobal-Liera <l24170599@culiacan.tecnm.mx>
2025-01-31 11:08:24 -0700

    Added last file from class

---
+++ src/logica/Alumno.java
@@ -0,0 +1,24 @@
+package logica;
+
+public class Alumno {
+  //Atributos
+  int numeroControl;
+  String nombre;
+  String apellido;
+  char genero;
+  String carrera;
+  int semestreActual;
+
+  //Operaciones
+  public float obtenerPromedio() {
+    return 0.0f;
+  }
+
+  public void cargarMateria() {
+    
+  }
+
+  public boolean cargarTicket(String problema) {
+    return false;
+  }
+}
